2a6304af5f17141ef886fba85b2a4f8254f0e1c2
cajex <Alexander.jilge@icloud.com>
2025-08-12 21:32:44 +0200

    remove jdocuments-doc-compiler

--- jdocuments-doc-compiler-client/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let config =
-        slint_build::CompilerConfiguration::new()
-            .with_style("cosmic-dark".into());
-    slint_build::compile_with_config("slint/doc-compiler.slint", config).unwrap();
-}
