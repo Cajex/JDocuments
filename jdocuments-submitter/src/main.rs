b8bdaefe35100c4c9c5521845d8efb03de47e925
cajex <Alexander.jilge@icloud.com>
2025-08-13 12:25:14 +0200

    Add clap parser for submitter.

--- jdocuments-submitter/src/main.rs
+++ jdocuments-submitter/src/main.rs
@@ -1,14 +1,35 @@
+use clap::{Parser, ValueEnum};
 use crate::storage::SubmitterStorage;
 use slint::ComponentHandle;
 
 mod storage;
 mod ui;
 
+#[derive(Parser, Debug)]
+#[command()]
+pub struct SubmitterArguments {
+    #[arg(value_enum, short_alias = 'm')]
+    mode: SubmitterMode,
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+pub enum SubmitterMode {
+    GraphicInterface,
+    Scanner,
+}
+
 fn main() {
+    let arguments = SubmitterArguments::parse();
+
     tracing_subscriber::fmt()
         .with_ansi(true)
         .with_thread_names(true)
         .init();
     let _storage = SubmitterStorage::default();
-    ui::setup().unwrap().run().unwrap()
+    match arguments.mode {
+        SubmitterMode::GraphicInterface => {
+            ui::setup().unwrap().run().unwrap()
+        }
+        SubmitterMode::Scanner => {}
+    }
 }
