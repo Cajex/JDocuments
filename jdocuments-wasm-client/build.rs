dcfae24becdb64fe043c12a3edec55d0affd6ed7
nuralex <Alexander.jilge@icloud.com>
2025-09-10 19:06:24 +0200

    Add ClientDashboardLinkNavigation and font for Dashboard UI.

--- jdocuments-wasm-client/build.rs
+++ jdocuments-wasm-client/build.rs
@@ -3,7 +3,7 @@ use slint_build::CompilerConfiguration;
 fn main() {
     slint_build::compile_with_config(
         "ui/client.slint",
-        CompilerConfiguration::new().with_style("cosmic-dark".to_owned()),
+        CompilerConfiguration::new().with_style("native".to_owned()),
     )
     .unwrap();
 }
