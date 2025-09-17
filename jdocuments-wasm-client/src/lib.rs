c56b106f3f7f70cf87109d29990e3c95ba854159
Alexander Jilge <Alexander.jilge@icloud.com>
2025-09-17 14:05:39 +0200

    Update SearchComponent. It is still work in progress.

--- jdocuments-wasm-client/src/lib.rs
+++ jdocuments-wasm-client/src/lib.rs
@@ -1,5 +1,8 @@
+use slint::{Color, Model, SharedString, VecModel};
 #[cfg(target_family = "wasm")]
 use wasm_bindgen::prelude::*;
+use log::debug;
+use slint::private_unstable_api::debug;
 
 slint::include_modules!();
 
@@ -7,5 +10,16 @@ slint::include_modules!();
     wasm_bindgen::prelude::wasm_bindgen(start))]
 pub fn main() {
     let main_window = TestWindow::new().unwrap();
+
+    SearchComponentGlobal::get(&main_window).on_push_part(|parts, part| {
+        debug(SharedString::from("Hello"));
+        let parts = parts.as_any().downcast_ref::<VecModel<(Color, SharedString)>>().unwrap();
+        let mut items = vec![];
+        for item in parts.iter() {
+            items.push(item);
+        }
+        parts.set_vec(items);
+    });
+
     main_window.run().unwrap();
 }
